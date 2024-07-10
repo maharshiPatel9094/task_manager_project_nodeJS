7e4813a55cccc3bd64fd603b1cc915c29ea56868
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-07-10 12:25:01 -0400

    connection esatblished

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # task_manager_project_nodeJS
  This project performs all the CRUD operations and get the client data and upload it in the cloud 
+
+
+# project start
+1. npm install
+
+# project routes for tasks
+        /api/v1/tasks
+1. "/" - getAllTasks - GET
+2. "/" - createTasks - POST
+3. "/:id" - getTask - GET
+4. "/:id" - updateTask - PATCH
+5. "/:id" - deleteTask - DELETE
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require("./db/db.config.js")
 const express = require("express")
 const app = express()
 const tasks = require("./routes/tasks.js")
 const {getAllTasks,createTasks,getTasks,updateTask,deleteTask} = require("./controllers/tasks.js")
+const connectDB = require("./db/db.config.js")
 
 
 
@@ -20,7 +20,16 @@ app.use("/api/v1/tasks", tasks)
 // port
 const PORT = 3000
 
-// app listening
-app.listen(PORT, ()=>{
-    console.log(`server is listening on ${PORT}`);
-})
+const start = async() => {
+    try {
+        await connectDB
+        // app listening
+        app.listen(PORT, ()=>{
+            console.log(`server is listening on ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
